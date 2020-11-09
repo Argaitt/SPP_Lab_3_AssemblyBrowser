34132069485c96b1ed78121d602096b8905e9d8e
argaitt <argaitt@gmail.com>
2020-11-09 19:29:29 +0300

    init

---
+++ AssemblyScanner/Scanner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Resources;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+
+namespace AssemblyScanner
+{ 
+    class InfoCell
+    {
+        internal string category;
+        internal string type;
+        internal string name;
+        internal List<InfoCell> InfoCells;
+        public  InfoCell()
+        {
+            InfoCells = new List<InfoCell>();
+        }
+    }
+    public class Scanner
+    {
+        private Assembly assembly;
+        public void AssemblyLoad(string assemblyFilePath)
+        {
+            assembly = Assembly.LoadFrom(assemblyFilePath);
+            
+        }
+        public object AssemblyScan()
+        {
+            InfoCell infoAssembly = new InfoCell();
+            infoAssembly.category = "Assembly";
+            infoAssembly.name = assembly.GetName().Name;
+            infoAssembly.type = assembly.GetType().ToString();
+
+            
+            Type[] types = assembly.GetTypes();
+            foreach (var item in types)
+            {
+               
+                if (infoAssembly.InfoCells.Find(namespaceItem => namespaceItem.name == item.Namespace) == null)
+                {
+                    InfoCell infoNamespace = new InfoCell();
+                    infoNamespace.name = item.Namespace;
+                    infoNamespace.category = "Namespace";
+                    infoNamespace.type = "None";
+                    infoAssembly.InfoCells.Add(infoNamespace);
+                }      
+            }
+
+            foreach (var item in types)
+            {
+                var element = infoAssembly.InfoCells.Find(namespaceItem => namespaceItem.name == item.Namespace);
+                InfoCell infoClass = new InfoCell();
+                infoClass.name = item.Name;
+                infoClass.category = "Class";
+                infoClass.type = "None";
+                element.InfoCells.Add(infoClass);
+                FieldInfo[] PropertysInfo = item.GetType().GetFields();
+                MethodInfo[] MethodsInfo = item.GetType().GetMethods(BindingFlags.DeclaredOnly);
+            }
+
+            object assemblyData = new object();
+            return assemblyData;
+        }
+        public void AssemblyUnload()
+        {
+            assembly = null;
+        }
+    }
+}
---
+++ CCP_Lab_1_AssemblyBrowser/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AssemblyScanner;
+
+namespace CCP_Lab_1_AssemblyBrowser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Scanner scanner = new Scanner();
+            scanner.AssemblyLoad("D:\\Bsuir\\SPP\\CPP_Lab_2_Faker\\CPP_Lab_2_Faker\\bin\\Debug\\netcoreapp3.1\\UsersClasses.dll");
+            scanner.AssemblyScan();
+        }
+    }
+}
